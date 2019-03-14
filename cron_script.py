ccbed8bb2258c1eadabc37a3739030c1ed04188a
chris <chris.gresock@gmail.com>
2019-03-14 15:44:12 -0400

    Added backup cron script

---
+++ cron_script.py
@@ -0,0 +1,25 @@
+import cv2, time, os
+
+output_path = "captures/cam{}"
+
+def get_cameras():
+    dev = os.listdir('/dev/')
+    out_cams = list()
+    for device in dev:
+        if "video" in device:
+            index = int(device[-1])
+            c = (index, cv2.VideoCapture(index))
+            out_cams.append(c)
+    return out_cams
+def check_path(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+def cam_capture():
+    cameras = get_cameras()
+    for index, camera in cameras:  
+        if camera.isOpened():
+            p = output_path.format(index)
+            check_path(p)
+            ret, frame = camera.read()
+            cv2.imwrite("{}/{}.jpg".format(p,time.time()), frame)
+            camera.release()
